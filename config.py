1ead31e3c248ad805342b9a4242deb3a2fc701ec
usmanxjavaid <218759122+usmanxjavaid@users.noreply.github.com>
2026-06-15 13:26:07 +0500

    Configuration file added

---
+++ config.py
@@ -0,0 +1,29 @@
+import os   
+from dotenv import load_env
+
+load_env() # it loads your .env file
+
+# LLM
+GROQ_API_KEY = os.getenv('GROQ_API_KEY', '')
+LLM_MODEL = 'llama3-8b-8192'
+LLM_TEMPERATURE = 0.1
+LLM_MAX_TOKENS = 1024
+
+# Embeddings
+EMBEDDING_MODEL = 'all-MiniLM-L6-v2'
+
+# Vectore Database
+CHROMA_DIR = './data/vectorstore'
+CHROMA_COLLECTION = 'documind'
+
+# Chunking
+CHUNK_SIZE = 800
+CHUNK_OVERLAP = 150
+
+# Retrieval 
+TOP_K = 4
+
+# File Uploads
+UPLOAD_DIR = './data/uploads'
+SUPPORTED_EXTENSIONS = ['.pdf', '.txt', 'docx']
+
